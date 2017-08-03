37b9f5d68d327ea2c8d92a087f312cd168a322f8
amyjzhu <ajyzhu@gmail.com>
2017-08-03 10:37:40 -0700

    Removed lingering .js files, I hope

--- src/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const Runner_1 = require("./Runner");
-let field = document.getElementById("pokemon");
-let button = document.getElementById("submit");
-let response = document.getElementById("response");
-let r = new Runner_1.default(field, button, response);
-button.onclick = () => { r.findPokemon(r); };
-module.exports = {};
